71e74ef7ae07c2c1c61996b731c6243204df6f5c
xesam <xesam@outlook.com>
2024-07-02 21:45:39 +0800

    mitt /dep

--- test/CounterTimer.test.js
+++ test/CounterTimer.test.js
@@ -3,22 +3,36 @@ jest.useFakeTimers('modern');
 
 describe('CountTimer', () => {
     it('normal counter', () => {
+        const counts = [];
         const timer = new CounterTimer(1000);
-        expect(timer.getCount()).toBe(0);
+        timer.on('tick', () => {
+            counts.push(timer.getCount());
+        });
+
         timer.start();
+        expect(counts).toStrictEqual([]);
+
         jest.advanceTimersByTime(1000);
-        expect(timer.getCount()).toBe(1);
-        jest.advanceTimersByTime(10000);
-        expect(timer.getCount()).toBe(11);
+        expect(counts).toStrictEqual([1]);
+
+        jest.advanceTimersByTime(9000);
+        expect(counts).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
     });
 
     it('counter with maxCount', () => {
+        const counts = [];
         const timer = new CounterTimer(1000, 5);
-        expect(timer.getCount()).toBe(0);
+        timer.on('tick', () => {
+            counts.push(timer.getCount());
+        });
+
         timer.start();
+        expect(counts).toStrictEqual([]);
+
         jest.advanceTimersByTime(1000);
-        expect(timer.getCount()).toBe(1);
-        jest.advanceTimersByTime(10000);
-        expect(timer.getCount()).toBe(5);
+        expect(counts).toStrictEqual([1]);
+
+        jest.advanceTimersByTime(9000);
+        expect(counts).toStrictEqual([1, 2, 3, 4, 5]);
     });
 });
